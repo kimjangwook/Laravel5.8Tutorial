770d7658cd520b5e94ce6f1efb77376f1d0a1a1c
Jangwook Kim <kjw1029.cognito@gmail.com>
2019-07-22 12:00:36 +0900

    36. 아티산 커맨드 생성 : 클로저
    routes/console.php에 아티산 커맨드 생성

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,13 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('company:clean', function () {
+    \App\Company::doesntHave('customerList')
+        ->get()
+        ->each(function ($company) {
+            $company->delete();
+            $this->warn('Company "'.$company->name.'" is removed.');
+        });
+    $this->info('Companies clean up has succeed.');
+})->describe('Cleans Up Unused Companies');
